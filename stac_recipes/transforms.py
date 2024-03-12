ecb1ad324312c05deee147bf65ac26eb915c92b5
Justus Magin <keewis@posteo.de>
2024-03-12 12:12:29 +0100

    implement a first draft of the `CreateStacItem` ptransform

--- stac_recipes/transforms.py
+++ stac_recipes/transforms.py
@@ -1,6 +1,32 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Callable
 
 import apache_beam as beam
+import pystac
+import xstac
+from tlz.functoolz import curry
+
+
+def passthrough(item, ds):
+    return item
+
+
+def create_stac_item(indexed, template, postprocess, xstac_kwargs=None):
+    if xstac_kwargs is None:
+        xstac_kwargs = {}
+
+    index, ds = indexed
+
+    if callable(template):
+        template = template(ds)
+
+    item = xstac.xarray_to_stac(
+        xstac.fix_attrs(ds),
+        template,
+        **xstac_kwargs,
+    )
+
+    return postprocess(item, ds)
 
 
 @dataclass
@@ -16,3 +42,20 @@ class Branch(beam.PTransform):
 class Passthrough(beam.PTransform):
     def expand(self, pcoll):
         return pcoll
+
+
+@dataclass
+class CreateStacItem(beam.PTransform):
+    template: pystac.Item | Callable
+    postprocess: Callable = passthrough
+    xstac_kwargs: dict = field(default_factory=dict)
+
+    def expand(self, pcoll):
+        return pcoll | "Create STAC item" >> beam.Map(
+            curry(
+                create_stac_item,
+                template=self.template,
+                postprocess=self.postprocess,
+                xstac_kwargs=self.xstac_kwargs,
+            )
+        )
