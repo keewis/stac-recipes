37d41c83440053d02e83bacee666f9b910bffcea
Justus Magin <keewis@posteo.de>
2024-03-18 14:27:02 +0100

    implement the parallel write to disk

--- stac_recipes/transforms.py
+++ stac_recipes/transforms.py
@@ -1,9 +1,11 @@
+import os
 from dataclasses import dataclass, field
 from typing import Callable
 
 import apache_beam as beam
 import pystac
 import xstac
+from pystac.utils import make_absolute_href, make_relative_href
 from tlz.functoolz import curry
 
 
@@ -163,4 +165,101 @@ class CreateCatalog(beam.PTransform):
     def expand(self, pcoll):
         return pcoll | "Create a root catalog" >> beam.CombineGlobally(
             CreateRootCatalog(template=self.template)
+        )
+
+
+def maybe_normalize_hrefs(cat, href, normalize=True):
+    cat.normalize_hrefs(href)
+
+    return cat
+
+
+def dehydrate_catalog(cat, dest_href, catalog_type):
+    if catalog_type == pystac.CatalogType.ABSOLUTE_PUBLISHED:
+        catalog_self_link = True
+    elif catalog_type != pystac.CatalogType.SELF_CONTAINED:
+        # root catalog gets a self link
+        catalog_self_link = True
+    else:
+        catalog_self_link = False
+
+    if dest_href is not None:
+        self_href = cat.get_self_href()
+        rel_href = make_relative_href(self_href, self_href)
+        href = make_absolute_href(rel_href, dest_href, start_is_dir=True)
+    else:
+        href = None
+
+    yield cat, href, catalog_self_link
+
+    for link in cat.get_child_links():
+        if not link.is_resolved():
+            continue
+
+        child = link.target
+
+        if dest_href is not None:
+            rel_href = make_relative_href(child.self_href, cat.self_href)
+            child_href = make_absolute_href(rel_href, dest_href, start_is_dir=True)
+        else:
+            child_href = None
+
+        yield from dehydrate_catalog(child, os.path.dirname(child_href), catalog_type)
+
+    item_self_link = catalog_type in [pystac.CatalogType.ABSOLUTE_PUBLISHED]
+    for link in cat.get_item_links():
+        if not link.is_resolved():
+            continue
+
+        item = link.target
+
+        if dest_href is not None:
+            rel_href = make_relative_href(item.self_href, cat.self_href)
+            item_href = make_absolute_href(rel_href, dest_href, start_is_dir=True)
+        else:
+            item_href = None
+
+        yield item, item_href, item_self_link
+
+
+def dehydrate(catalog, dest_href, catalog_type):
+    if catalog.get_root() is not catalog:
+        raise ValueError("can only dehydrate using the root catalog")
+
+    cat = catalog.clone()
+    cat.type = catalog_type
+
+    yield from dehydrate_catalog(cat, dest_href, catalog_type)
+
+
+def store_as_json(data):
+    obj, href, include_self_link = data
+
+    stac_io = pystac.StacIO.default()
+
+    obj.save_object(
+        dest_href=href, include_self_link=include_self_link, stac_io=stac_io
+    )
+
+    return href
+
+
+@dataclass
+class ToStaticJson(beam.PTransform):
+    href: str
+    normalize: bool = True
+    catalog_type: pystac.CatalogType = pystac.CatalogType.RELATIVE_PUBLISHED
+
+    def expand(self, pcoll):
+        return (
+            pcoll
+            | "Normalize hrefs"
+            >> beam.Map(
+                curry(maybe_normalize_hrefs, href=self.href, normalize=self.normalize)
+            )
+            | "Dehydrate"
+            >> beam.FlatMap(
+                dehydrate, catalog_type=self.catalog_type, dest_href=self.href
+            )
+            | "Write to disk" >> beam.Map(store_as_json)
         )
