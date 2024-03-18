e8d727cbf6a7e3166235976e2788adc60cc65464
Justus Magin <keewis@posteo.de>
2024-03-18 11:57:43 +0100

    refactor to support a `from_sequence` constructor

--- stac_recipes/patterns.py
+++ stac_recipes/patterns.py
@@ -1,19 +1,34 @@
 from dataclasses import dataclass
+from typing import Callable, Sequence
 
-import pandas as pd
 from pangeo_forge_recipes.patterns import FileType
+from tlz.itertoolz import identity
 
 
 @dataclass
 class FilePattern:
-    path_fn: callable
-    time: pd.Series
+    path_fn: Callable
+    sequence: Sequence
     file_type: FileType | str
+    key_formatter: Callable | str = identity
 
     def __post_init__(self):
         if isinstance(self.file_type, str):
             self.file_type = FileType(self.file_type)
+        if isinstance(self.key_formatter, str):
+            formatters = {
+                "isoformat": lambda ts: ts.isoformat(),
+            }
+            formatter = formatters.get(self.key_formatter)
+            if formatter is None:
+                raise ValueError(f"unknown key formatter: {self.key_formatter}")
+
+            self.key_formatter = formatter
+
+    @classmethod
+    def from_sequence(cls, sequence, file_type, **kwargs):
+        return cls(identity, sequence, file_type, **kwargs)
 
     def items(self):
-        for ts in self.time:
-            yield ts.isoformat(), self.path_fn(ts)
+        for item in self.sequence:
+            yield self.key_formatter(item), self.path_fn(item)
